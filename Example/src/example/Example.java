2160d356c7b395d1d6bedf78fee115422736d521
Xavier595 <japlmo03@gmail.com>
2025-11-26 21:22:08 +0100

    Added array and print

--- Example/src/example/Example.java
+++ Example/src/example/Example.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package example;
-
+import java.util.Arrays;
 /**
  *
  * @author javie
@@ -15,7 +15,8 @@ public class Example {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello");
+        int[] table = {4, 56, 8, 0, 1};
+        System.out.println("Table: " + Arrays.toString(table));
     }
     
 }
