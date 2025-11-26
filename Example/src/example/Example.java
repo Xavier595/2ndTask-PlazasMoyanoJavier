cc7cf2bbc53c99e557415ff2152f97bb250ce5ad
Xavier595 <japlmo03@gmail.com>
2025-11-26 21:30:47 +0100

    Added random values

--- Example/src/example/Example.java
+++ Example/src/example/Example.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package example;
+
 import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author javie
@@ -15,8 +18,12 @@ public class Example {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] table = {4, 56, 8, 0, 1};
-        System.out.println("Table: " + Arrays.toString(table));
+        int[] table = new int[5];
+        Random random = new Random();
+        for (int i = 0; i < table.length; i++) {
+            table[i] = random.nextInt(100);
+        }
+        System.out.println("Random values: " + Arrays.toString(table));
     }
-    
+
 }
